b552aa0ee040250ae255684580f3bdf4abe27bb8
siemen_subbaiah <siemen.s@iqnext.io>
2024-10-24 00:18:57 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'coffee-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/screens/home-screen/home-screen.component').then(
+        (m) => m.HomeScreenComponent
+      ),
+  },
+  {
+    path: 'cafes',
+    loadComponent: () =>
+      import(
+        './restaurants/screens/restaurant-home-screen/restaurant-home-screen.component'
+      ).then((m) => m.RestaurantHomeScreenComponent),
+  },
+];

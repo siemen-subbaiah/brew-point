aec70058fc56730d5c788947dc7fd439079e5059
siemen_subbaiah <siemen.s@iqnext.io>
2024-10-24 00:22:50 +0530

    added auth screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/auth/screens/login-screen/login-screen.component').then(
+        (m) => m.LoginScreenComponent
+      ),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./home/screens/home-screen/home-screen.component').then(
